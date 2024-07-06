2ccf293b201f501606e67de51be3e4cc75133820
Stephanie Carr <sacarr91@gmail.com>
2024-07-06 18:40:16 -0400

    starting over

--- public/scripts/404.js
+++
@@ -1,8 +0,0 @@
-const goHomeBtn = document.getElementById('start-over');
-
-const goHome = (e) => {
-  e.preventDefault();
-  window.location.href = '/';
-};
-
-goBackBtn.addEventListener('click', goHome);
