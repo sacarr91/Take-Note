e3d513e161142429c37248769d03cf03b73509ff
Stephanie Carr <sacarr91@gmail.com>
2024-07-07 21:31:51 -0400

    updated server.js

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,6 @@ const router = require('express').Router();
 
 const notesRouter = require('./notes');
 
+router.use('/notes', notesRouter);
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,2 +1,26 @@
 const express = require('express');
-const api = require('./routes');
+const path = require('path');
+const api = require('./routes/index.js');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(`${__dirname}/public/index.html`));
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(`${__dirname}/public/pages/notes.html`));
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
